f10c43ab4987f183212927e7a9c436b4136017de
Miheer Tamkhane <54612063+MiheerTamkhane@users.noreply.github.com>
2022-02-05 17:55:38 +0530

    modal comp added

---
+++ modal/modal.js
@@ -0,0 +1,15 @@
+const modalBtn = document.querySelector("#modal-btn");
+const ctModal = document.querySelector("#ct-modal");
+const ctModalParent = document.querySelector(".ct-modal-dialog");
+const modalClose = document.querySelector("#modal-close");
+const body = document.querySelector("body");
+
+modalBtn.addEventListener("click", () => {
+  ctModal.classList.add("ct-modal-active");
+  ctModalParent.classList.add("ct-dialog-active");
+});
+
+modalClose.addEventListener("click", () => {
+  ctModal.classList.remove("ct-modal-active");
+  ctModalParent.classList.remove("ct-dialog-active");
+});
